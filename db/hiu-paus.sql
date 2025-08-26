89f99fa8a1b6fd6b9cb8991d8455dd692c813ddf
dkcpllmd <dukcapil.lamandau@gmail.com>
2025-08-26 20:38:01 +0700

    db

--- db/hiu-paus.sql
+++ db/hiu-paus.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Jul 22, 2025 at 08:08 AM
+-- Generation Time: Aug 26, 2025 at 01:37 PM
 -- Server version: 8.4.3
 -- PHP Version: 8.3.22
 
@@ -20,6 +20,35 @@ SET time_zone = "+00:00";
 --
 -- Database: `hiu-paus`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `payment_details`
+--
+
+CREATE TABLE `payment_details` (
+  `id` int NOT NULL,
+  `user_id` int UNSIGNED DEFAULT NULL,
+  `full_name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `kontak` varchar(20) NOT NULL,
+  `jumlah_orang` int NOT NULL,
+  `total_biaya` int NOT NULL,
+  `role_payment` enum('pending','confirmed','completed') DEFAULT 'pending',
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `payment_details`
+--
+
+INSERT INTO `payment_details` (`id`, `user_id`, `full_name`, `email`, `kontak`, `jumlah_orang`, `total_biaya`, `role_payment`, `created_at`, `updated_at`) VALUES
+(4, 1, 'Muhammad Fikri Ridwan', 'mfikryrid@gmail.com', '089786756464', 2, 1300000, 'pending', '2025-08-20 01:35:20', '2025-08-20 01:35:20'),
+(5, 1, 'Muhammad Fikri Ridwan', 'lamandau.piak@gmail.com', '089786756464', 2, 1300000, 'pending', '2025-08-20 01:52:01', '2025-08-20 01:52:01'),
+(6, 1, 'Muhammad Fikri Ridwan', 'super@lamandau.online', '089786756464', 5, 3250000, 'pending', '2025-08-20 01:53:44', '2025-08-20 01:53:44'),
+(7, 1, 'Jacob Adflin', 'super@lamandau.online', '082250706412', 1, 650000, 'pending', '2025-08-22 00:07:45', '2025-08-22 00:07:45');
 
 -- --------------------------------------------------------
 
@@ -44,11 +73,19 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `username`, `password`, `nama_lengkap`, `kontak`, `email`, `role_user`, `created_at`, `updated_at`) VALUES
-(1, 'user', '123', 'User', '08123456789', 'john@example.com', '1', '2025-07-14 07:43:51', '2025-07-22 08:08:08');
+(1, 'user', '123', 'User', '08123456789', 'john@example.com', '1', '2025-07-14 07:43:51', '2025-07-22 08:08:08'),
+(2, 'admin', '123', 'Administrator', '-', 'admin@gmail.com', '2', '2025-07-25 12:23:02', '2025-07-25 12:23:02');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `payment_details`
+--
+ALTER TABLE `payment_details`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`);
 
 --
 -- Indexes for table `users`
@@ -61,10 +98,26 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `payment_details`
+--
+ALTER TABLE `payment_details`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
   MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `payment_details`
+--
+ALTER TABLE `payment_details`
+  ADD CONSTRAINT `payment_details_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
