0e52079231ac66058afd938c72d769861d1d3b60
mfrdw <139567650+mfrdw@users.noreply.github.com>
2025-09-09 22:54:10 +0700

    Update hiu-paus.sql

--- db/hiu-paus.sql
+++ db/hiu-paus.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Aug 31, 2025 at 04:49 PM
+-- Generation Time: Sep 09, 2025 at 03:53 PM
 -- Server version: 8.0.30
 -- PHP Version: 8.1.29
 
